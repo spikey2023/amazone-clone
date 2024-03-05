2297aafe99eaebe8676a72ad28420f554b69f02d
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-05 10:49:59 -0700

    Updated background styling and removed global styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material";
 import theme from "@/styles/theme";
+import { COLORS } from "@/styles/colors";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body 
+        className={inter.className} 
+        style={{backgroundColor: COLORS.lightGray, margin: 0}}
+        >
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-'use client';
-
-import { useSupabase } from "@/hooks/useSupabase";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import React, { useEffect } from "react";
 
 
 const Home = () => {
-  const { products, getProducts } = useSupabase()
-
-  useEffect(() => {
-    getProducts();
-    // console.log(products);
-  }, [getProducts]);
-
-
   return (
     <Box>
       <Typography variant="h1">Home</Typography>
