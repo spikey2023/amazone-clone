097dec4192376f64fc2bd967c1460aaf23bb2d5f
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-05 10:32:41 -0700

    Added additional colors

---
+++ styles/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+    darkBlue: "#131921" ,
+    mediumBlue: "#232f3e",
+    white : "#ffffff",
+    lightGray:"#e3e6e6",
+    paleOrange:"#febd69",
+    teal:"#25a3ff",
+    darkOrange: "#f08805",
+    green: "#007600",
+    red: "#b83c1d",
+    yellow : "#ffd814",
+    orange : "#f3a946",
+}
