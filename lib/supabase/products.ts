d4963b40cf377a773e06d19b58edaba7d8f9a2fd
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-05 09:59:32 -0700

    updated supbase keys

--- lib/supabase/products.ts
+++ lib/supabase/products.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+//supbase needs RLDS security turned off to fech data or needs the secret private key
 export const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!, 
-    process.env.NEXT_PUBLIC_SECRET_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 )
