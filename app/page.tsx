ce2ae04832127500c477661a87ec6ee605dedc71
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-04 20:00:31 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Home</Typography>
+    </Box>
+  );
+};
+
+export default Home;
