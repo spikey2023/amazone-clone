428063f2ed865629c4a3d6d5ec3aa2761dda3362
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-09 10:21:55 -0700

    Adde image and styling productCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,38 @@
-import { Box } from '@mui/material'
+import { COLORS } from '@/styles/colors'
+import { Box, Typography } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 
 const ProductCard = ({product}: {product:any}) => {
   return (
-    <Box>{product.title} {product.price}</Box>
+    <Box 
+        sx={{
+            backgroundColor: COLORS.white, 
+            width: 225, 
+            height: 450, 
+            margin: "0.5rem",
+            padding: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent:"space-between"
+            }}
+    >
+        <Image 
+            alt={product.title} 
+            src={product.image}
+            width={200}
+            height={250}  
+        />
+        <Typography 
+            variant='h2' 
+            sx={{
+                fontWeight:500,}}>
+            {product.title.substring(0,15)}... 
+        </Typography>
+        <Typography >
+            {product.price}
+        </Typography>
+    </Box>
   )
 }
 
